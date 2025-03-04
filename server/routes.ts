a9ee08d23bd73f5e67afdf0c34ec4681201bdcbc
Emie <emie.vandeveire@student.arteveldehs.be>
2025-03-04 17:41:50 +0100

    Made detail page and working index

--- server/routes.ts
+++ server/routes.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import { title } from "process";
+import { getNews, getNewsBySlug, addNews } from "./data/newsService";
 
 const router = express.Router();
 
 /**
  * GET / - Laadt de homepagina
  */
+
 router.get("/", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  const news = getNews();
+  res.render("index", { title: "Alle nieuwsberichten", news: news});
+});
+
+router.get("/detail/:slug", (req: Request, res: Response): void => {
+  const slug: string = req.params.slug;
+  const article = getNewsBySlug(slug);
+
+  res.render("detail", { title: "Gekozen nieuwsbericht", article: article });
+
 });
 
 export default router;
